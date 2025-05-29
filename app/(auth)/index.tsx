dd8f488fdac24ab44e9435099e7a981a8f0dc0c7
Adjei <samensah036@st.ug.edu.gh>
2025-05-29 16:39:07 +0000

    Add configuration for NativeWind and TailwindCSS
    - Created a log file to track progress and configuration steps.
    - Added metro.config.js to integrate NativeWind with Expo.
    - Updated package.json to include NativeWind and TailwindCSS dependencies.
    - Adjusted versions of react-native-reanimated and react-native-safe-area-context.
    - Created tailwind.config.js to set up TailwindCSS with NativeWind presets.

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AuthIndex = () => {
+  return (
+    <View>
+      <Text>Auth index</Text>
+    </View>
+  );
+};
+
+export default AuthIndex;
---
+++ app/(chat)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const index = () => {
+  return (
+    <View>
+      <Text>Chat index</Text>
+    </View>
+  );
+};
+
+export default index;
