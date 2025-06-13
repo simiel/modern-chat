151748a72378cb0ea7720f0752241370b0fe8bc6
Adjei <samensah036@st.ug.edu.gh>
2025-06-13 00:47:38 +0000

    feat: enhance Chat component with loading state and error handling; improve UI structure and header configuration

--- app/(chat)/[chat].tsx
+++ app/(chat)/[chat].tsx
@@ -1,24 +1,71 @@
-import { useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { ActivityIndicator, SafeAreaView, Text } from 'react-native';
 
 const Chat = () => {
   const { chat: chatId } = useLocalSearchParams();
+  const [messages, setMessages] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState('');
+
+  if (!chatId) {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+        <Text
+          style={{
+            flex: 1,
+            textAlign: 'center',
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginTop: 20,
+            color: '#fff',
+          }}
+        >
+          Chat Room Not Found
+        </Text>
+      </SafeAreaView>
+    );
+  }
+
+  if (loading) {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+        <ActivityIndicator
+          size="large"
+          color="#fff"
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        />
+      </SafeAreaView>
+    );
+  }
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
-      <Text
-        style={{
-          flex: 1,
-          textAlign: 'center',
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginTop: 20,
-          color: '#fff',
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerBackTitle: 'Back',
+          title: `Chat Room: ${chatId}`,
+          headerStyle: { backgroundColor: '#000' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' },
+          headerTitleAlign: 'center',
         }}
-      >
-        {chatId ? `Chat Room: ${chatId}` : 'Chat Room Not Found'}
-      </Text>
-    </SafeAreaView>
+      />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+        <Text
+          style={{
+            flex: 1,
+            textAlign: 'center',
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginTop: 20,
+            color: '#fff',
+          }}
+        >
+          {chatId ? `Chat Room: ${chatId}` : 'Chat Room Not Found'}
+        </Text>
+      </SafeAreaView>
+    </>
   );
 };
 
